ab05488a1992c83dda04c53b11476c15b98fab33
Srikant <usp71084@gmail.com>
2012-01-12 02:32:39 +0530

    Added todo in client.py

---
+++ client.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# file: client.py
+# desc: will scan the client machine for dependencies like motion server, python modules etc.
+#       Should send binary/hex file to Arduino device(or any target) as soon as it receives the 
+#       file from server. The file will be received and overwritten each time when technician
+#       executes the program on HTML form. So need to monitor constantly for any updation in file.
+
+
+
