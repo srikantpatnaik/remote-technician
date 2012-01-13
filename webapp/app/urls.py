acbc1c1c5e4f4adf8371d54c43b4d7e440818bf7
psachin <iclcoolster@gmail.com>
2012-01-13 20:20:20 +0530

    testing: webapp/app added

---
+++ webapp/app/urls.py
@@ -0,0 +1,20 @@
+from django.conf.urls.defaults import patterns, include, url
+from app.views import hello, current_datetime 
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+                       ('^hello/$', hello),
+                       ('^time/$', current_datetime)
+
+    # Examples:
+    # url(r'^$', 'app.views.home', name='home'),
+    # url(r'^app/', include('app.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
